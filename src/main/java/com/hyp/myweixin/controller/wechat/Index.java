69f680e90abb9af2f9646632a89baab8a9a4d565
heyapei <15518901416@163.com>
2020-05-27 11:21:56 +0800

    创建微信后端项目

---
+++ src/main/java/com/hyp/myweixin/controller/wechat/Index.java
@@ -0,0 +1,22 @@
+package com.hyp.myweixin.controller.wechat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 何亚培
+ * @version V1.0
+ * @Date 2020/5/27 11:06
+ * @Description: TODO
+ */
+@Controller
+@Slf4j
+public class Index {
+
+    @RequestMapping("/")
+    public String redirect() {
+        log.info("用户想要请求首页被跳转到www.yapei.cool");
+        return "redirect:http://www.yapei.cool";
+    }
+}
